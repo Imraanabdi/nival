02fb92e7829f9faf4e9930782bb1dcf4c143c4c7
imranabdisalan7 <agmraan@gmail.com>
2024-05-27 12:03:00 +0300

    rock paper scissors

--- trial.py
+++ trial.py
@@ -7,7 +7,23 @@ player = None
 
 while player not in choices:
     player = input("rock, paper, or scissors? ")
-
+if player == computer:
+    print("Tie!")
+elif player == "rock":
+    if computer == "paper":
+        print("You lose!", computer, "covers", player)
+    else:
+        print("You win!", player, "smashes", computer)
+elif player == "paper":
+    if computer == "scissors":
+        print("You lose!", computer, "cuts", player)
+    else:
+        print("You win!", player, "covers", computer)
+elif player == "scissors":
+    if computer == "rock":
+        print("You lose...", computer, "smashes", player)
+    else:
+        print("You win!", player, "cut", computer)
 print(computer)
 print(player)
-
+print("You lost")
